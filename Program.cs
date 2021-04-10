d168d3d7f0f715628a7a5f4542f0d931f0921b6c
SirKotoff <68810705+SirKotoff@users.noreply.github.com>
2021-04-10 21:36:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace String_last_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Данные считаны из файла Inlet.in]");
+            Str_file();
+            Thread.Sleep(1500);
+            Console.WriteLine("[Данные записаны в файл Outlet.out]");
+            Console.ReadKey();
+
+        }
+
+
+
+        static void Str_file()
+        {
+            string[] text;
+            string res = null;
+            using (StreamReader streamreader = new StreamReader("Inlet.in"))
+            {
+                text = streamreader.ReadToEnd().Trim().Split(' ','\r');
+            }
+          
+            
+            for (int i = 0; i < text.Length; i++)
+            {
+                char[] array = text[i].ToCharArray();
+                for (int j = 0; j <= array.Length-2; j++)
+                {
+                    if (array[j] == array[array.Length-1])
+                    {
+                        array[j] = '_';
+                       
+                    }
+                    
+                   
+                }
+                res += new string(array) + " ";
+            }
+            File.WriteAllText(("Outlet.out"), res);
+  
+        }
+    }
+}
+//by Alex Kotyukov
